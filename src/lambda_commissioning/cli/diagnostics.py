49df807f4d3e523a0be9f1e6cef957c8d5246e1e
JaidenCook <jaiden.cook1@gmail.com>
2026-01-06 09:00:48 +0800

    Added if statements to plotting to catch zero value visibilities and autos.

--- src/lambda_commissioning/cli/diagnostics.py
+++ src/lambda_commissioning/cli/diagnostics.py
@@ -21,7 +21,7 @@ directoryDict = config.get("paths", {})
 dataPath = directoryDict['dataPath']
 outPath = directoryDict['outputPath']
 
-diagnosticApp = typer.Typer()
+diagnosticApp = typer.Typer(pretty_exceptions_enable=False)
 
 @diagnosticApp.command()
 def file_header(filename: Annotated[str,typer.Argument(help="Data filename.")] = ""):
@@ -42,6 +42,7 @@ def autos(filename: Annotated[str,typer.Argument(help="Data filename.")] = "",
                          typer.Option("-v","--verbose",help="Test optional arg.")] = False,
           channel: Annotated[int,
                             typer.Option("-c","--channel",help="Starting channel of the observation.")] = None):
+    # TODO: Save the statistics to a csv file.
     # Creating the output directory name.
     outputDir = outPath + f"{filename.split('.')[0]}/"
     outputAutosDir = outputDir + "autocorrelations/"
@@ -116,30 +117,39 @@ def autos(filename: Annotated[str,typer.Argument(help="Data filename.")] = "",
         stdYY = iqr(waterFallYY)/1.35
         avgYY = np.nanmedian(waterFallYY)
         waterFallYY[waterFallYY > 3*stdYY+avgYY] = np.nan
+
+        # Check the data is not zero. If all zeros then truth value of stats 
+        # will be False. If any stat value is True we can generate plots,
+        # else the data is all zero and we pass, printing the erronous ant ID.
+        if np.any(autoXXmeanVec[i,:]) and np.any(autoXXstdVec[i,:]):
+            #
+            fig,axs = plt.subplots(1,figsize=(2*W,W),sharex=True,
+                                constrained_layout=True)
+            outFileNameXX = f"auto_correlation_ant{antInd}_polXX.png"
+            
+            waterfallPlot(waterFallXX,cmap=cmap,title=f'AntID: {antInd}, XX',
+                        figaxs=(fig,axs))
+            fig.savefig(outputAutosDir+outFileNameXX,dpi=300,bbox_inches='tight')
+            plt.close()
+
+            if verbose:
+                print(outputAutosDir+outFileNameXX)
+        else:
+            print(f"XX waterfall is zero for {antInd}")
         
         #
-        fig,axs = plt.subplots(1,figsize=(2*W,W),sharex=True,
-                            constrained_layout=True)
-        outFileNameXX = f"auto_correlation_ant{antInd}_polXX.png"
-        waterfallPlot(waterFallXX,cmap=cmap,title=f'AntID: {antInd}, XX',
-                    figaxs=(fig,axs))
-        fig.savefig(outputAutosDir+outFileNameXX,dpi=300,bbox_inches='tight')
-        plt.close()
-
-        #
-        fig,axs = plt.subplots(1,figsize=(2*W,W),sharex=True,
-                            constrained_layout=True)
-        outFileNameYY = f"auto_correlation_ant{antInd}_polYY.png"
-        waterfallPlot(waterFallYY,cmap=cmap,title=f'AntID: {antInd}, YY',
-                    figaxs=(fig,axs))
-        fig.savefig(outputAutosDir+outFileNameYY,dpi=300,bbox_inches='tight')
-        plt.close()
-
-        if verbose:
-            print(outputAutosDir+outFileNameXX)
-            print(outputAutosDir+outFileNameYY)
-
-        
+        if np.any(autoYYmeanVec[i,:]) and np.any(autoYYstdVec[i,:]):
+            fig,axs = plt.subplots(1,figsize=(2*W,W),sharex=True,
+                                constrained_layout=True)
+            outFileNameYY = f"auto_correlation_ant{antInd}_polYY.png"
+            waterfallPlot(waterFallYY,cmap=cmap,title=f'AntID: {antInd}, YY',
+                        figaxs=(fig,axs))
+            fig.savefig(outputAutosDir+outFileNameYY,dpi=300,bbox_inches='tight')
+            plt.close()
+            if verbose:
+                print(outputAutosDir+outFileNameYY)
+        else:    
+            print(f"YY waterfall is zero for {antInd}")
         
 
 @diagnosticApp.command()
@@ -215,33 +225,41 @@ def vis(filename: Annotated[str,typer.Argument(help="Data filename.")] = "",
         visWaterfallXX[visWaterfallXX > 2*stdXX+avgXX] = np.nan
         visWaterfallPhaseXX[visWaterfallXX > 2*stdXX+avgXX] = np.nan
         
-        W = 8
-        cmap = cmr.dusk
+        if np.any(avgXX) and np.any(stdXX):
+            W = 8
+            cmap = cmr.dusk
 
-        fig,axs = plt.subplots(1,figsize=(2*W,W),sharex=True,
-                                constrained_layout=True)
-        waterfallPlot(visWaterfallXX,cmap=cmap,title=titleXX,
-                      figaxs=(fig,axs))
-        outFileNameXXamp = f"vis_amp_waterfall_ant1_{ant1}_ant2_{ant2}_polXX.png"
-        fig.savefig(outputAmpDir+outFileNameXXamp,dpi=300,bbox_inches='tight')
-        plt.close()
-            
-        #
-        fig,axs = plt.subplots(1,figsize=(2*W,W),sharex=True,
-                                constrained_layout=True)
-        waterfallPlot(visWaterfallPhaseXX,cmap=cmr.wildfire,title=titleXX,
-                      figaxs=(fig,axs),phaseCond=True)
-        outFileNameXXphase = f"vis_phase_waterfall_ant1_{ant1}_ant2_{ant2}_polXX.png"
-        fig.savefig(outputPhaseDir+outFileNameXXphase,dpi=300,bbox_inches='tight')
-        plt.close()
+            fig,axs = plt.subplots(1,figsize=(2*W,W),sharex=True,
+                                    constrained_layout=True)
+            waterfallPlot(visWaterfallXX,cmap=cmap,title=titleXX,
+                        figaxs=(fig,axs))
+            outFileNameXXamp = f"vis_amp_waterfall_ant1_{ant1}_ant2_{ant2}_polXX.png"
+            fig.savefig(outputAmpDir+outFileNameXXamp,dpi=300,bbox_inches='tight')
+            plt.close()
+                
+            #
+            fig,axs = plt.subplots(1,figsize=(2*W,W),sharex=True,
+                                    constrained_layout=True)
+            waterfallPlot(visWaterfallPhaseXX,cmap=cmr.wildfire,title=titleXX,
+                        figaxs=(fig,axs),phaseCond=True)
+            outFileNameXXphase = f"vis_phase_waterfall_ant1_{ant1}_ant2_{ant2}_polXX.png"
+            fig.savefig(outputPhaseDir+outFileNameXXphase,dpi=300,bbox_inches='tight')
+            plt.close()
+        
+            #
+            fig,axs = plt.subplots(1,figsize=(10,5))
+            fringePlot(corrTensorXX[:,0,ant1,ant2],figaxs=(fig,axs))
+            axs.set_title(titleXX)
+            outFileNameXXfringe = f"vis_fringe_ant1_{ant1}_ant2_{ant2}_polXX.png"
+            fig.savefig(outputFringeDir+outFileNameXXfringe,dpi=300,bbox_inches='tight')
+            plt.close()
 
-        #
-        fig,axs = plt.subplots(1,figsize=(10,5))
-        fringePlot(corrTensorXX[:,0,ant1,ant2],figaxs=(fig,axs))
-        axs.set_title(titleXX)
-        outFileNameXXfringe = f"vis_fringe_ant1_{ant1}_ant2_{ant2}_polXX.png"
-        fig.savefig(outputFringeDir+outFileNameXXfringe,dpi=300,bbox_inches='tight')
-        plt.close()
+            if verbose:
+                print(outputAmpDir+outFileNameXXamp)
+                print(outputPhaseDir+outFileNameXXphase)
+                print(outputFringeDir+outFileNameXXfringe)
+        else:
+            print(f"XX vis is zero for {antInd}")
 
         #
         visWaterfallYY = np.abs(corrTensorYY[:,:,ant1,ant2])
@@ -252,35 +270,37 @@ def vis(filename: Annotated[str,typer.Argument(help="Data filename.")] = "",
         visWaterfallPhaseYY[visWaterfallYY > 2*stdYY+avgYY] = np.nan
         
         #
-        fig,axs = plt.subplots(1,figsize=(2*W,W),sharex=True,
-                                constrained_layout=True)
-        waterfallPlot(visWaterfallYY,cmap=cmap,title=titleYY,
-                      figaxs=(fig,axs))
-        outFileNameYYamp = f"vis_amp_waterfall_ant1_{ant1}_ant2_{ant2}_polYY.png"
-        fig.savefig(outputAmpDir+outFileNameYYamp,dpi=300,bbox_inches='tight')
-        plt.close()
+        if np.any(avgYY) and np.any(stdYY):
+            fig,axs = plt.subplots(1,figsize=(2*W,W),sharex=True,
+                                    constrained_layout=True)
+            waterfallPlot(visWaterfallYY,cmap=cmap,title=titleYY,
+                        figaxs=(fig,axs))
+            outFileNameYYamp = f"vis_amp_waterfall_ant1_{ant1}_ant2_{ant2}_polYY.png"
+            fig.savefig(outputAmpDir+outFileNameYYamp,dpi=300,bbox_inches='tight')
+            plt.close()
 
-        #
-        fig,axs = plt.subplots(1,figsize=(2*W,W),sharex=True,
-                                constrained_layout=True)
-        waterfallPlot(visWaterfallPhaseYY,cmap=cmr.wildfire,title=titleYY,
-                      figaxs=(fig,axs),phaseCond=True)
-        outFileNameYYphase = f"vis_phase_waterfall_ant1_{ant1}_ant2_{ant2}_polYY.png"
-        fig.savefig(outputPhaseDir+outFileNameYYphase,dpi=300,bbox_inches='tight')
-        plt.close()
+            #
+            fig,axs = plt.subplots(1,figsize=(2*W,W),sharex=True,
+                                    constrained_layout=True)
+            waterfallPlot(visWaterfallPhaseYY,cmap=cmr.wildfire,title=titleYY,
+                        figaxs=(fig,axs),phaseCond=True)
+            outFileNameYYphase = f"vis_phase_waterfall_ant1_{ant1}_ant2_{ant2}_polYY.png"
+            fig.savefig(outputPhaseDir+outFileNameYYphase,dpi=300,bbox_inches='tight')
+            plt.close()
+            
+            #
+            fig,axs = plt.subplots(1,figsize=(10,5))
+            fringePlot(corrTensorYY[:,0,ant1,ant2],figaxs=(None,axs))
+            axs.set_title(titleYY)
+            outFileNameYYfringe = f"vis_fringe_ant1_{ant1}_ant2_{ant2}_polYY.png"
+            fig.savefig(outputFringeDir+outFileNameYYfringe,dpi=300,bbox_inches='tight')
+            plt.close()
+
+            if verbose:
+                print(outputAmpDir+outFileNameYYamp)
+                print(outputPhaseDir+outFileNameYYphase)
+                print(outputFringeDir+outFileNameYYfringe)
+        else:
+            print(f"YY vis is zero for {antInd}")
+
         
-        #
-        fig,axs = plt.subplots(1,figsize=(10,5))
-        fringePlot(corrTensorYY[:,0,ant1,ant2],figaxs=(None,axs))
-        axs.set_title(titleYY)
-        outFileNameYYfringe = f"vis_fringe_ant1_{ant1}_ant2_{ant2}_polYY.png"
-        fig.savefig(outputFringeDir+outFileNameYYfringe,dpi=300,bbox_inches='tight')
-        plt.close()
-
-        if verbose:
-            print(outputAmpDir+outFileNameXXamp)
-            print(outputPhaseDir+outFileNameXXphase)
-            print(outputFringeDir+outFileNameXXfringe)
-            print(outputAmpDir+outFileNameYYamp)
-            print(outputPhaseDir+outFileNameYYphase)
-            print(outputFringeDir+outFileNameYYfringe)
